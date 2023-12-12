875fae1d09fce1e152cfa5ca77540ab550cf9955
TaahaaGul <tahagulll06@gmail.com>
2023-12-13 02:32:38 +0300

    implementing Retry pattern in Accounts ./

--- accounts/src/main/java/com/taahaagul/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/taahaagul/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.taahaagul.accounts.dto.CustomerDto;
 import com.taahaagul.accounts.dto.ErrorResponseDto;
 import com.taahaagul.accounts.dto.ResponseDto;
 import com.taahaagul.accounts.service.IAccountsService;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -22,6 +25,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Taha Gul
@@ -35,6 +40,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @Validated
 public class AccountsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsController.class);
 
     private final IAccountsService iAccountsService;
 
@@ -200,11 +207,21 @@ public class AccountsController {
                     )
             )
     })
+    @Retry(name= "getBuildInfo", fallbackMethod="getBuildInfoFallback")
     @GetMapping("/build-info")
-    public ResponseEntity<String> getBuildInfo() {
+    public ResponseEntity<String> getBuildInfo() throws TimeoutException {
+        logger.debug("Inside getBuildInfo method");
+        throw new TimeoutException();
+//        return ResponseEntity
+//                .status(HttpStatus.OK)
+//                .body(buildVersion);
+    }
+
+    public ResponseEntity<String> getBuildInfoFallback(Throwable throwable) {
+        logger.debug("Inside getBuildInfoFallback method");
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(buildVersion);
+                .body("0.9");
     }
 
     @Operation(

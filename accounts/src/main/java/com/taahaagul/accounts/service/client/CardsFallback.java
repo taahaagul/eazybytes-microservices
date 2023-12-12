6bc8aa3c6e4357c7df5c61dee554bbb2f7ea0ea6
TaahaaGul <tahagulll06@gmail.com>
2023-12-12 18:26:35 +0300

    Implementing Circuit Breaker Pattern with Feign Client ./

---
+++ accounts/src/main/java/com/taahaagul/accounts/service/client/CardsFallback.java
@@ -0,0 +1,14 @@
+package com.taahaagul.accounts.service.client;
+
+import com.taahaagul.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
---
+++ accounts/src/main/java/com/taahaagul/accounts/service/client/LoansFallback.java
@@ -0,0 +1,14 @@
+package com.taahaagul.accounts.service.client;
+
+import com.taahaagul.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
